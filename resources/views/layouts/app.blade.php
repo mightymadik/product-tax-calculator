291713c3eeea047a4ee26b3adab08ea740a8d1e2
Мадияр Бижанов <madiyar.biz@svoydom.kz>
2025-02-05 10:28:34 +0500

    Стили

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Tax Calculator</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.css" rel="stylesheet" />
 </head>
+
 <body>
-    <div class="container">
+    <div class="flex flex-col items-center justify-center wrapper max-w-xl mx-auto h-screen">
         @yield('content')
     </div>
+
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
 </html>
